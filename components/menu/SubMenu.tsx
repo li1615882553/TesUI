b8f6bf1c99568a223a5fb1f0eab2966a222494f4
A_Bo <li_student@163.com>
2022-08-29 18:02:46 +0800

    完善menu组件

--- components/menu/SubMenu.tsx
+++ components/menu/SubMenu.tsx
@@ -2,6 +2,7 @@ import { Control, Component, VNode } from "tes-work";
 import { IBaseComponent } from "../template/component";
 import classNames from "classnames";
 import Menu from "./Menu";
+import Popup from "@component/popup/popup";
 export interface ISubMenuProps extends IBaseComponent {
   /**submenu ID */
   index: string;
@@ -9,30 +10,75 @@ export interface ISubMenuProps extends IBaseComponent {
   disabled?: boolean;
   /**标题 */
   title: string | VNode;
+  /**展开 sub-menu 的延时 */
+  showTime?: number;
+  /**收起 sub-menu 的延时 */
+  hideTime?: number;
 }
 
 @Component
 export class SubMenu extends Control<ISubMenuProps>{
   rootMenu: Menu;
   deep: number = 0;
-  childMenuVisible: boolean = false;
+  timeout;
+  mouseInChild: boolean = false;
+  get opend() {
+    const { index } = this.props;
+    return this.rootMenu.openedMenus.indexOf(index) !== -1;
+  }
 
-  openChange() {
+  handleOpen(e: MouseEvent) {
+    e.stopPropagation();
+    const { mode = "vertical", menuTrigger = "hover", } = this.rootMenu.props;
     const { disabled, index } = this.props;
-    if (disabled) return;
-    if (this.rootMenu.handleOpenChange) {
-      this.rootMenu.handleOpenChange(this);
-    }
+
+    if ((menuTrigger === "hover" && mode === "horizontal") || disabled) return;
+    this.rootMenu.handleSubmenuClick(this);
+  }
+
+  handleMouseEnter(e: MouseEvent) {
+    const { disabled, showTime = 300, index } = this.props;
+    const { mode = "vertical", menuTrigger = "hover", } = this.rootMenu.props;
+    if ((menuTrigger === "click" && mode === "horizontal") || disabled) return;
+
+    Object.keys(this.rootMenu.subMenus).forEach(subMenu => {
+      if(this.rootMenu.subMenus[subMenu].deep !== this.deep){
+        console.log("触发enter")
+        this.rootMenu.subMenus[subMenu].mouseInChild = true;
+        clearTimeout(this.rootMenu.subMenus[subMenu].timeout);
+      }
+    });
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      this.rootMenu.openMenu(index);
+    }, showTime);
+  }
+  handleMouseLeave(e: MouseEvent) {
+    const { disabled, hideTime = 100, index } = this.props;
+    const { mode = "vertical", menuTrigger = "hover", } = this.rootMenu.props;
+    if ((menuTrigger === "click" && mode === "horizontal") || disabled) return;
+
+    console.log("触发leave")
+    Object.keys(this.rootMenu.subMenus).forEach(subMenu => {
+      console.log(this.rootMenu.subMenus[subMenu].deep , this.deep)
+      if(this.rootMenu.subMenus[subMenu].deep === this.deep){
+        this.rootMenu.subMenus[subMenu].mouseInChild = false;
+        clearTimeout(this.rootMenu.subMenus[subMenu].timeout);
+      }
+    });
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      !this.mouseInChild && this.rootMenu.closeMenu(index);
+    }, hideTime);
   }
 
   protected componentWillMount() {
-    const { index } = this.props;
     let parent = this.$parent;
     while (parent) {
       if (parent instanceof Menu) {
         this.rootMenu = parent;
         this.deep++;
-        this.childMenuVisible = this.rootMenu.openedMenus.indexOf(index) !== -1;
+        this.rootMenu.addSubMenu(this);
         break;
       } else if (parent instanceof SubMenu) {
         this.deep++;
@@ -40,43 +86,66 @@ export class SubMenu extends Control<ISubMenuProps>{
       parent = parent.$parent;
     }
   }
+  protected componentWillDestory(): void {
+    this.rootMenu.removeSubMenu(this);
+  }
 
   render() {
-    const { className, style, title, index, disabled } = this.props;
+    const { className, style, title, disabled } = this.props;
     const { mode = "vertical", offset = 24 } = this.rootMenu.props;
     const clsName = classNames(
       "t-submenu", className,
       {
-        [`is-open`]: this.childMenuVisible,
+        [`is-open`]: this.opend,
         [`is-disabled`]: disabled,
       },
     );
-    const paddingLeft = mode === "vertical" ? `${this.deep! * offset}px` : '';
+    const paddingLeft = mode === "vertical" ? `${this.deep * offset}px` : '';
     const action = mode === "vertical" ?
       {
-        onClick: this.openChange
+        onClick: this.handleOpen
       } : {
-        onMouseEnter: () => { this.childMenuVisible = true },
-        onMouseLeave: () => { this.childMenuVisible = false }
+        onMouseEnter: this.handleMouseEnter,
+        onMouseLeave: this.handleMouseLeave
       };
+    const titleContent = (
+      <div
+        className="t-submenu__title"
+        style={`padding-left:${paddingLeft};`}
+      >
+        {title}
+        <i class={`t-submenu__arrow iconfont icon-downArrow`}></i>
+      </div>
+    );
+
     return (
       <li
         className={clsName}
+        style={style}
         {...action}
       >
-        <div
-          className="t-submenu__title"
-          style={`"padding-left:${paddingLeft}"`}
-        >
-          {title}
-          <i class={`t-submenu__arrow iconfont ${this.childMenuVisible ? "icon-downArrow" : "icon-upArrow"}`}></i>
-        </div>
         {
-          this.childMenuVisible ?
-            (<ul className="t-menu">
-              {this.$children}
-            </ul>)
-            : null
+          mode === "horizontal" ?
+            (
+              <Popup
+                type="visible"
+                align={ this.deep === 1 ? "bottom" : "rr-cc" }
+                visible={this.opend}
+                style="position:absolute;"
+                margin={0}
+                content={<ul className="t-menu t-menu--popup" onMouseEnter={this.handleMouseEnter} onMouseLeave = {this.handleMouseLeave} >{this.$children}</ul>}
+              >
+                {titleContent}
+              </Popup>
+            ) :
+            (
+              <div>
+                { titleContent }
+                <ul className="t-menu" style={`display:${this.opend ? '' : 'none'}`}>
+                  {this.$children}
+                </ul>
+              </div>
+            )
         }
       </li>
     )
--- example/menu/app.tsx
+++ example/menu/app.tsx
@@ -3,21 +3,55 @@ import { Component, Control, Watch, nextTick } from "tes-work";
 import Menu from "@component/menu/Menu";
 import SubMenu from "@component/menu/SubMenu";
 import MenuItem from "@component/menu/MenuItem";
+import MenuItemGroup from "@component/menu/MenuItemGroup";
+
+import Row from "@component/grid/row";
+import Col from "@component/grid/col";
 
 @Component
 class Panel extends Control {
+  handleSelect(index) {
+    console.log(index)
+  }
   render() {
     return (
       <div>
-        <Menu defaultActive="1-1">
-          <SubMenu index="1" title="导航一">
-            <MenuItem index="1-1">处理中心</MenuItem>
-            <MenuItem index="1-2">我的工作台</MenuItem>
-          </SubMenu>
-          <SubMenu index="2" title="导航二">
-            <MenuItem index="1-3" disabled>消息中心</MenuItem>
-          </SubMenu>
-        </Menu>
+        <Row>
+          <Col span={4}>
+            <Menu ref={this.addRefs("menu")} defaultActive="1-1" onSelect={this.handleSelect}>
+              <SubMenu index="1" title="导航一">
+                <MenuItemGroup title="分组一">
+                  <MenuItem index="1-1">处理中心</MenuItem>
+                  <MenuItem index="1-2"><i className="iconfont icon-loading"></i> 我的工作台</MenuItem>
+                </MenuItemGroup>
+                <SubMenu index="1-3" title={<span><i className="iconfont icon-add"></i>关于</span>}>
+                  <MenuItem index="1-3-1">我的</MenuItem>
+                  <MenuItem index="1-3-2">历史记录</MenuItem>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu index="2" title="导航二">
+                <MenuItem index="1-3" disabled>消息中心</MenuItem>
+              </SubMenu>
+            </Menu>
+          </Col>
+          <Col span={8}>
+            <Menu mode="horizontal" onSelect={this.handleSelect}>
+              <SubMenu index="1" title="导航一">
+                <MenuItemGroup title="分组一">
+                  <MenuItem index="1-1">处理中心</MenuItem>
+                  <MenuItem index="1-2"><i className="iconfont icon-loading"></i> 我的工作台</MenuItem>
+                </MenuItemGroup>
+                <SubMenu index="1-3" title={<span><i className="iconfont icon-add"></i>关于</span>}>
+                  <MenuItem index="1-3-1">我的</MenuItem>
+                  <MenuItem index="1-3-2">历史记录</MenuItem>
+                </SubMenu>
+              </SubMenu>
+              <SubMenu index="2" title="导航二">
+                <MenuItem index="1-3" disabled>消息中心</MenuItem>
+              </SubMenu>
+            </Menu>
+          </Col>
+        </Row>
       </div>
     )
   }
